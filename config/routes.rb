acfe2e90162a6517cd38783fe3670417f6241732
Jan Niewielski <39317379+jniewiel@users.noreply.github.com>
2024-06-25 17:16:27 +0000

    added new Movie routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,16 +5,19 @@ Rails.application.routes.draw do
 
   # Routes for the Movie resource:
 
+  get("/movies/:id/edit", controller: "movies", action: "edit")
+
   # CREATE
   post("/movies", { :controller => "movies", :action => "create" })
 
   # READ
   get("/movies", { :controller => "movies", :action => "index" })
 
+  get("/movies/new", { :controller => "movies", :action => "new" })
+
   get("/movies/:id", { :controller => "movies", :action => "show" })
 
   # UPDATE
-
   patch("/movies/:id", { :controller => "movies", :action => "update" })
 
   # DELETE
